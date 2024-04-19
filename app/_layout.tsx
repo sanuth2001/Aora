7d02f7b390e960c768a34955c104ed26a37e1373
Sanuth Dasanyaka <sanuthsasmin7@gmail.com>
2024-04-19 10:36:12 +0530

    Splashcreen add

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,8 +1,24 @@
 import { StyleSheet, Text, View } from 'react-native';
-import { Slot, Stack } from 'expo-router';
+import { Slot, SplashScreen, Stack } from 'expo-router';
+import { useFonts } from 'expo-font';
+import { useEffect } from 'react';
+
+SplashScreen.preventAutoHideAsync();
 
 
 const RooyLayout = () => {
+  const [fontsLoaded,error]  = useFonts({
+    "poppins-Black": require("../assets/fonts/Poppins-Black.ttf")
+  })
+
+  useEffect(()=>{
+    if(error) throw error;
+    if(fontsLoaded) SplashScreen.hideAsync();
+  },
+    [fontsLoaded,error])
+    
+    if(!fontsLoaded && !error) return null;
+ 
   return (
    <Stack>
     <Stack.Screen name='index' options={{headerShown:false}}/>
